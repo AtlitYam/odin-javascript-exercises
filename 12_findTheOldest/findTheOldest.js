f83f582262441c156997d7d743a8fccce807e2b6
wilcojongens <wilco.jongens@topicus.nl>
2023-03-21 18:26:14 +0100

    Rewrite 12 Complete 13

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,17 +1,23 @@
-const findTheOldest = function (people) {
-    let oldestPerson
-    let oldestPersonAge = 0
-    for (const person of people) {
+const findTheOldest = (people) => people.reduce((oldest, person) => calculateAge(oldest) < calculateAge(person) ? person : oldest)
 
-        let currentPersonAge = (person.yearOfDeath ? person.yearOfDeath : new Date().getFullYear()) - person.yearOfBirth
-        console.log(currentPersonAge)
-        if (currentPersonAge > oldestPersonAge) {
-            oldestPerson = person
-            oldestPersonAge = currentPersonAge
-        }
-    }
-    return oldestPerson
-};
+function calculateAge(person) {
+    return person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : new Date().getFullYear() - person.yearOfBirth
+}
+
+// const DEPRECATEDfindTheOldest = function (people) {
+//     let oldestPerson
+//     let oldestPersonAge = 0
+//     for (const person of people) {
+
+//         let currentPersonAge = (person.yearOfDeath ? person.yearOfDeath : new Date().getFullYear()) - person.yearOfBirth
+//         console.log(currentPersonAge)
+//         if (currentPersonAge > oldestPersonAge) {
+//             oldestPerson = person
+//             oldestPersonAge = currentPersonAge
+//         }
+//     }
+//     return oldestPerson
+// };
 
 // Do not edit below this line
 module.exports = findTheOldest;
--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,11 @@
-const caesar = function() {
+const caesar = (string, shift) => string.split('').map((char) => caesarShift(char, shift, isCapital(char))).join('')
+const caesarShift = (char, shift, isCapital) => isLetter(char) ? String.fromCharCode(mod(char, shift, isCapital)) : char
 
-};
+const isLetter = (char) => char.match(/[A-z]/)
+const isCapital = (char) => char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90
+const codeCorrection = (isCapital) => isCapital ? 65 : 97
+const correctShift = (shift) => shift < 0 ? shift + 26 : shift
+const mod = (char, shift, isCapital) => ((((char.charCodeAt(0) - codeCorrection(isCapital)) + correctShift(shift)) % 26) + codeCorrection(isCapital))
 
 // Do not edit below this line
 module.exports = caesar;
